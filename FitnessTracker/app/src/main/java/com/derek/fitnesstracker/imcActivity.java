2f7bd1056464cf09904858bf8ab356e82817c20e
derek-alves <derek.alves1998@hotmail.com>
2021-02-15 11:01:56 -0300

    First navegation

---
+++ FitnessTracker/app/src/main/java/com/derek/fitnesstracker/imcActivity.java
@@ -0,0 +1,14 @@
+package com.derek.fitnesstracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class imcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imc);
+    }
+}
