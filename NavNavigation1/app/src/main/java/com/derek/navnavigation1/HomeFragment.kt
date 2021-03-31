d48aedf87af686e1fff94fcc75cec3950e63975e
derek-alves <derek.alves1998@hotmail.com>
2021-03-31 10:33:30 -0300

    Navigation graph

---
+++ NavNavigation1/app/src/main/java/com/derek/navnavigation1/HomeFragment.kt
@@ -0,0 +1,22 @@
+package com.derek.navnavigation1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.derek.navnavigation1.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+private lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_home,container,false)
+        return binding.root
+    }
+
+
+}
